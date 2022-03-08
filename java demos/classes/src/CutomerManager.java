8cfda621d686dfedada832027699646676646354
aziz şimşek <98578225+02AzizSimsek@users.noreply.github.com>
2022-03-08 21:35:23 +0300

    Add files via upload

---
+++ java demos/classes/src/CutomerManager.java
@@ -0,0 +1,16 @@
+public class CutomerManager {
+    //buraya methodalari yazariz
+    //musteriyi ilgilendiren methodlari yazariz
+    //buraya operasyonlari katariz
+
+    public void Add(){
+        //mesala buraya musteri ekleme kodlarini eklememiz lazim
+    }
+    public void Remove(){
+        //mesala buraya musteri silme kodlarini eklememiz lazim
+
+    }
+    public void Update(){
+        //mesala buraya musteri bilgi guncelleme kodlarini eklememiz lazim
+    }
+}
---
+++ java demos/classes/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        CutomerManager manager = new CutomerManager();   //cagirma islemini bu sekilde yapariz,CutomerManager sinifin ismi,manager kismi isim kismidir ve bize kalmis oradaki isimlendirme sekl
+        manager.Add();
+        manager.Remove();
+        manager.Update();
+//class lar refarans tiptir
+
+    }
+
+}
+// classlarin ilk ve temel ozelligi gruplama yapmaktir
+//ilgili alanlarda class olustururuz
+//diziler referans tiptir,
